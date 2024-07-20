b5b57500ae4adad0b372eb9e94cb0b2c5dada29e
unknown <karaut0403@gmail.com>
2024-07-21 05:29:11 +0530

    updates in ML model

--- model.py
+++ model.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Random Forest Classification
 
 # ## Importing the libraries
-
-# In[1]:
 
 
 import numpy as np
@@ -16,7 +11,6 @@ import pickle
 
 # ## Importing the dataset
 
-# In[2]:
 
 
 dataset = pd.read_csv('Crop_recommendation.csv')
@@ -24,21 +18,10 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 
-# In[3]:
-
-
-print(X)
-
-
-# In[4]:
-
-
-print(y)
 
 
 # # Encoding the Dependent Variable
 
-# In[5]:
 
 
 from sklearn.preprocessing import LabelEncoder
@@ -46,43 +29,27 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 
 
-# In[6]:
-
-
-print(y)
-
 
 # ## Splitting the dataset into the Training set and Test set
 
-# In[7]:
-
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-
-# In[8]:
 
 
 print("X Train Size:",len(X_train))
 print(X_train)
 
 
-# In[9]:
-
 
 print("X Test Size:",len(X_test))
 print(X_test)
 
 
-# In[10]:
 
 
 print("Y Train Size:",len(y_train))
 print(y_train)
 
-
-# In[11]:
 
 
 print("X Test Size:",len(y_test))
@@ -93,8 +60,6 @@ print(y_test)
 
 # ## Training the Random Forest Classification model on the Training set
 
-# In[12]:
-
 
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
@@ -102,8 +67,6 @@ classifier.fit(X_train, y_train)
 
 
 # ## Making the Confusion Matrix
-
-# In[13]:
 
 
 from sklearn.metrics import confusion_matrix, accuracy_score
